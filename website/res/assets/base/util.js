8c6249552b8df0b1f1b7abf7ed5356c6aae7e17f
novalagung <caknopal@gmail.com>
2022-09-10 14:58:06 +0700

    fix: unsafe jquery plugin

--- website/res/assets/base/util.js
+++ website/res/assets/base/util.js
@@ -96,7 +96,7 @@
 
 			// Expand "target" if it's not a jQuery object already.
 				if (typeof config.target != 'jQuery')
-					config.target = $(config.target);
+					config.target = $.find(config.target);
 
 		// Panel.
 
