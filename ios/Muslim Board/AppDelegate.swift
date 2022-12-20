c7b3ddc89a5762cb4ca59c0dc4fc0dfe0c422762
megadestina <megadestina@gmail.com>
2022-12-20 12:36:34 +0700

    feat: add xcodeproject for safari extension

---
+++ ios/Muslim Board/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Muslim Board
+//
+//  Created by Mega Destina on 20/12/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
---
+++ ios/Muslim BoardUITests/Muslim_BoardUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Muslim_BoardUITestsLaunchTests.swift
+//  Muslim BoardUITests
+//
+//  Created by Mega Destina on 20/12/22.
+//
+
+import XCTest
+
+class Muslim_BoardUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
